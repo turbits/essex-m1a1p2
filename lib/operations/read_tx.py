f1fb3323715c233edced67b86bbb79d91ad6cd2d
Trevor Woodman <trevor.woodman@whitehorse.ca>
2022-08-03 21:31:01 +0000

    update header comment

--- lib/operations/read_tx.py
+++ lib/operations/read_tx.py
@@ -1,10 +1,11 @@
 # +===================================================================+
 # Author: Trevor Woodman
 # Github: https://github.com/turbits
+# Repo: https://github.com/turbits/essex-m1a1p2
 # Project: Assignment 1, Part 2: Bankbook Program
 # Course: Launching into Computer Science (LCS_PCOM7E June 2022)
 # School: University of Essex
-# Date: July 27th, 2022
+# Date: July/August, 2022
 # +===================================================================+
 
 from .. import io
